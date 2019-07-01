d11be97607da3fd43e60d8a25bc5837dee136317
Nicolas Kolling Ribas <nicolaskribas@gmail.com>
2019-07-01 16:51:07 -0300

    Adicionado a população da lista de transições (simbolos terminais).

--- dfa-generator.py
+++ dfa-generator.py
@@ -1,28 +1,36 @@
 AFND = []
+states = []
+transitions = []
+def newCollumn():
+    for i in range(len(AFND)):
+        AFND[i].append([])
 
 def addRG(line):
     print("Devia adicionar '"+line+"'ao automato");
     #implementar
 
 def addToken(line):
+    state = 'S'
     for symbol in line:
-        i =1
+        if symbol not in transitions and '\n' not in symbol:
+            newCollumn()
+            transitions.append(symbol)
+
     print("Adicionando '"+line+"'ao automato")
 
 
 
 def main():
     file = open("text.txt","r")
+    states.append('S')  #Adiciona o estado inicial S
+    AFND.append([]) #Adiciona linha na tabela
     for line in file:
         if line[0] == '<':  #Caso seja um Gramatica Regular ela é adicionada ao Automato
             addRG(line)
         else:
             addToken(line)  #Caso seja um Token ele é adicionado ao Automato
-
+    print(states)
+    print(transitions)
+    print(AFND)
     file.close()
-    AFND.append([])
-    AFND.append([])
-    AFND[0].append('A')
-    AFND[1].append('B')
-    print(AFND[1][0])
 main()
